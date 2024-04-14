fab10671715da452afc9e91aac965166100d699d
Ritwika <ritwika2004@gmail.com>
2024-04-15 01:14:26 +0530

    changed

--- frontend/src/Components/Item/Item.jsx
+++ frontend/src/Components/Item/Item.jsx
@@ -2,18 +2,18 @@ import React from 'react'
 import './Item.css'
 import { assets } from '../../assets/assets'
 
-const Item = (id,name,price,description,image) => {
+const Item = ({id,name,price,description,image}) => {
   return (
     <div className='item'>
         <div className="item-container">
-            <img src={image} className='item-image' />
+            <img src={image} className='item-image' alt={name} />
         </div>
         <div className="item-info">
             <div className="rating">
                 <p>{name}</p>
-                <img src={assets.rating_starts} />
+                <img src={assets.rating_starts}  alt="Rating" />
             </div>
-            <p className="item-description"> {description} </p>
+            <p className="item-description">{description}</p>
             <p className="item-price">{price}</p>
         </div>
     </div>
