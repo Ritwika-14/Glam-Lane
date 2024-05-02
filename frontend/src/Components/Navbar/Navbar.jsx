a0eeb7b84b8e326c411c69ebb108e98eeda9a9b7
Ritwika <ritwika2004@gmail.com>
2024-05-02 10:35:47 +0530

    added

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = ({setshowLogin}) => {
 
   return (
     <div className='navbar'>
-        <img src={assets.logo} alt="" className='navbar-logo'/>
+        <Link to='/'><img src={assets.logo} alt="" className='navbar-logo'/></Link>
         <ul className="navbar-menu">
           <Link to='/' onClick={()=>setMenu("home")} className={menu === "home"?"active":""}>Home</Link>
           <a href='#menu' onClick={()=>setMenu("menu")} className={menu === "menu"?"active":""}>Menu</a>
@@ -18,7 +18,7 @@ const Navbar = ({setshowLogin}) => {
         <div className="navbar-right">
           <img src={assets.search_icon} alt="" />
           <div className="navbar-search-icon">
-            <img src={assets.basket_icon} alt="" />
+            <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
             <div className="dot"></div>
           </div>
           <button onClick={()=>setshowLogin(true)}>Sign-in</button>
--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Cart.css'
+import { StoreContext } from '../../context/StoreContext';
 
 const Cart = () => {
+
+  const {cartItems,product_list,removeFromcart} = useContext(StoreContext);
+  
   return (
-    <div>Cart</div>
+    <div>
+      <div className="cart-items">
+        <div className="cart-items-title">
+          <p>Items</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
+        </div>
+        <br />
+        <hr />
+        {product_list.map((item,index)=>{
+          if(cartItems[item._id]>0)
+          {
+            return(
+              <div>
+              <div className="cart-items-title cart_item_image">
+                <img src={item.image} />
+                <p>{item.name}</p>
+                <p>{item.price}</p>
+                <p>{cartItems[item._id]}</p>
+                <p>{item.price*cartItems[item._id]}</p>
+                <p>X</p>
+              </div>
+              <hr />
+              </div>
+            )
+          }
+        })}
+      </div>
+    </div>
   )
 }
 
